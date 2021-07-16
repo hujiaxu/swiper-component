759da0fbefc0c449b5bd017b8a7b94b85e8555d1
hujx <hujx@zylliondata.com>
2021-07-16 14:37:57 +0800

    完成当前公司(zy)业务功能

---
+++ swiper.vue
@@ -0,0 +1,167 @@
+
+<template>
+    <div class="swiper">
+        <div
+            class="swiper-container"
+            @touchstart="touchstart"
+            @touchmove="touchmove"
+            @touchend="touchend"
+        >
+            <div 
+                class="swiper-item"
+                :class="activeIndex === index ? 'swiper-item-active' : ''"
+                v-for="(item, index) in swiperArr"
+                :key="index"
+            >
+            </div>
+        </div>
+        <div class="dots">
+            <div 
+                class="dot"
+                :class="activeIndex === index ? 'dot-active' : ''"
+                v-for="(item, index) in swiperArr"
+                :key="index"
+            >
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SwiperComponent',
+        data() {
+            return {
+                swiperArr: new Array(5),
+                activeIndex: 0
+            }
+        },
+        mounted() {
+            this.el = document.querySelector('.swiper-container')        // 容器
+            this.itemEls = document.querySelectorAll('.swiper-item')     // 元素
+            this.width = this.itemEls[0].offsetWidth                     // 元素宽度，每次滑动的位移
+            this.length = this.itemEls.length                            // 元素的数量
+            this.style = this.el.style                                   // 元素的样式，用于在滑动过程中改变，易于操作
+            this.left = 0                                                // 每次滑动开始计算 容器元素 距 视图的左边距
+            this.minLeft = 0                                             // 左极限值，向右滑动时，容器元素 距 视图的最小左边距
+            this.maxLeft = (this.length - 1) * this.width                // 右极限值，向左滑动时，容器元素 距 视图的最大左边距
+            this.state = 0                                               // 判断状态，防止 意外触发 touchmove
+            this.X = 0                                                   // 初始值，记录手指 touchstart 时的 x坐标
+            this.moveX = 0                                               // 过程变化值，手指滑动时 相对 this.X 的 偏移距离
+            this.moveFlag = this.width / 3                               // 偏移的闸阀，超过这个值才会进行 整个 width 距离的偏移，否则回到原位
+        },
+        methods: {
+            // 在 touchstart,touchmove,touchend三个函数中
+            // 自变量：this.moveX，this.X
+            // 因变量：this.left，this.style.left，this.activeIndex，
+            touchstart(e) {
+                this.state = 1
+                this.left = this.el.offsetLeft
+                const { clientX } = e.touches[0]
+                this.X = clientX
+            },
+            touchmove(e) {
+                if (this.state !== 1) {
+                    return
+                }   
+                this.moveX = e.touches[0].clientX - this.X
+                this.style.left = this.left + this.moveX + 'px'             
+            },
+            touchend() {
+                if (this.moveX > 0) {
+                    if (this.moveX < this.moveFlag) {
+                        this.style.left = this.left + 'px'
+                    } else if (this.moveX >= this.moveFlag) {
+                        let step = Math.ceil(this.left / this.width)
+                        step++
+                        this.itemEls[0].style.marginLeft = 0 + 'px'
+                        if (this.left <= 0 && Number.parseFloat(this.style.left) >= 0) {
+                            this.style.left = 0 + 'px'
+                        } else {
+                            this.style.left = this.width * step + 'px'
+                            this.activeIndex = Math.abs(step)
+                        }
+                    }
+                }
+
+                if (this.moveX < 0) {
+                    const moveX = Math.abs(this.moveX)
+                    const left = Math.abs(this.left)
+                    if (moveX < this.moveFlag) {
+                        this.style.left = this.left + 'px'
+                    } else if (moveX >= this.moveFlag) {
+                        let step = Math.ceil(left / this.width)
+                        step++
+                        this.itemEls[0].style.marginLeft = '0.1%'
+                        if (left >= this.maxLeft && Math.abs(Number.parseFloat(this.style.left)) >= this.maxLeft) {
+                            this.style.left = this.left + 'px'
+                        } else {
+                            this.style.left = -step * this.width + 'px'
+                            this.activeIndex = step
+                        }
+                    }
+                }
+            }
+        },
+    }
+</script>
+
+<style>
+    body {
+        padding: 0;
+        margin: 0;
+    }
+</style>
+<style scoped>
+        /* overflow: hidden; 没有加overflow: hidden; 为什么swiper的高度会很低 */
+    .swiper {
+        overflow: hidden;
+        position: relative;
+        margin: 0 auto;
+        height: 600px;
+        padding: 30px 0;
+        width: 50vw;
+        background: #fff;
+    }
+    .swiper .swiper-container {
+        position: relative;
+        display: flex;
+        align-items: center;
+        left: 0;
+        width: 10000%;
+        padding: 15px 0;
+        transition: left .2s ease;
+    }
+    .swiper .swiper-container .swiper-item {
+        width: 0.9%;
+        margin-right: 0.05%;
+        background: #acacac;
+        height: 270px;
+        transition: all .2s ease;
+    }
+    .swiper .swiper-container .swiper-item-active {
+        height: 300px;
+        background: #adadad;
+        box-shadow: 2px 2px 19px rgba(0, 0, 0, 0.3);
+    }
+    .swiper .dots {
+        position: absolute;
+        top: 370px;
+        left: 50%;
+        transform: translateX(-50%);
+        display: flex;
+        justify-content: space-around;
+        width: 50%;
+        height: 20px;
+    }
+    .swiper .dots .dot {
+        width: 20px;
+        height: 20px;
+        border-radius: 10px;
+        background: #ccc;
+    }
+    .swiper .dots .dot-active {
+        width: 40px;
+        background: #aaa;
+    }
+</style>
